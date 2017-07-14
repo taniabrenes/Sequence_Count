07fca453ed37ca1277ead95fa2e0b783bfd4429f
TBrenes <taniabrenes@gmail.com>
2017-07-13 18:57:59 -0700

    changed parser to argparse and added a positional argument for the file names. This code still does not work well with the std.in

--- count_sequences4.py
+++ count_sequences4.py
@@ -10,7 +10,7 @@ Input read1 + read2 of a fastq sequence, or a tab delimited file.
 
 import gzip
 import sys
-from optparse import OptionParser
+import argparse
 
 def read_1fastq_sequence(ri):
         ID = ri.readline().strip()
@@ -19,63 +19,59 @@ def read_1fastq_sequence(ri):
         qc = ri.readline().strip()
         return [ID, Seq, nn, qc]
 
-parser = OptionParser()
-parser.add_option("-q", "--quite", action = "store_false",
+parser = argparse.ArgumentParser(description = 'Count sequences in a fastq file')
+parser.add_argument("reads", nargs = '*', default = sys.stdin,
+                    help = "File names for the fastq sequences")
+parser.add_argument("-q", "--quiet", action = "store_false",
                   help = "Do not print sequence to screen.", 
                   dest = "verbose", default = True)
-parser.add_option("-o", "--output", action = "store",
+parser.add_argument("-o", "--output", action = "store",
                   help = "Save sequence count to a text file.", 
                   dest = "outfile", default = "none")
 
-(options, args) = parser.parse_args()
-
-pyargs = len(args)
+args = parser.parse_args()
 
 n = 0
 
-if pyargs == 0:
-    r1 = sys.stdin
+if type(args.reads) == file:
+    r1 = args.reads[0]
 
-##  If there are file names in args, open input files
+    ## Check if stdin file is already tab delimited 
+    l1 = r1.readline().strip()
+    r1.seek(0)
+    
+    if len(l1.split('\t')) > 1:
+        for line in r1:
+            n += 1
+            if args.verbose:
+                sys.stdout.write(line)
+    else:
+        sys.exit("Stdin input must be tab delimited\n")
+
+##  If there are file names in reads, open input files
+##  and convert to tab delimited.
 else:
-    r1f = args[0]
+    r1f = args.reads[0]
     if r1f.split('.')[-1] == 'gz':
         r1 = gzip.open(r1f)
     else:
         r1 = open(r1f)
-
-    if pyargs == 2:
-        r2f = args[1]
-        if r1f.split('.')[-1] == 'gz':
-            r2 = gzip.open(r2f)
-        else:
-            r2 = open(r1f)
+    
+    r2f = args.reads[1]
+    if r1f.split('.')[-1] == 'gz':
+        r2 = gzip.open(r2f)
     else:
-        sys.exit("Error: Wrong input. Please enter file names for read 1 and read 2")
+        r2 = open(r1f)
 
-## Check if stdin file is already tab delimited 
-ID1 = r1.readline().strip()
-r1.seek(0)
-
-if len(ID1.split('\t')) > 1:
-    for line in r1:
-        n += 1
-        if options.verbose:
-            sys.stdout.write(line)
-
-## If file is not tab delimited convert to tab and pipe to console.
-else:
-    # Check data. 
-    # make sure data is entered as a fastq sequence
+    # Check data.
     (ID1, Seq1, nn1, qc1) = read_1fastq_sequence(r1)
     (ID2, Seq2, nn2, qc2) = read_1fastq_sequence(r2)
     
     # checks for the symbol + to make sure files are fastq format
     if nn1 != "+" or nn2 != "+":
-        sys.exit( "Read files may not be in fastq format.\n")
+        sys.exit( "Read files must be in fastq format.\n")
 
-    # compare the seq id of r1 and r1 to make sure they match. It compares 
-    # lines only up to pos 39 because after that there name has the read #
+    # compare the seq id of r1 and r1 to make sure they match. 
     if ID1[:39] != ID2[:39]:
         sys.exit("Sequence IDs do not match between reads.\n")
 
@@ -92,13 +88,13 @@ else:
             break
     
         n += 1
-        if options.verbose:
+        if args.verbose:
             line  = '\t'.join([ID1, Seq1, qc1, Seq2, qc2 + "\n"])
             sys.stdout.write( line )
 
 # output the sequence count 
-if options.outfile != "none":
-    out = open (options.outfile, "a") 
+if args.outfile != "none":
+    out = open (args.outfile, "a") 
     out.write("\nCount = %s\n" % n)
     out.close()
 else:
